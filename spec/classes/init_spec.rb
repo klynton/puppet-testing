ee216b476e24b027a12b66ea8c9529c99603e32d
Klynton Jessup <klynton@puppetlabs.com>
2014-09-16 22:50:31 -0700

    Add in a basic spec.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 describe 'testing' do
-
   context 'with defaults for all parameters' do
     it { should contain_class('testing') }
+    it { should contain_class('git')     }
+    it { should contain_class('nginx')   }
   end
 end
