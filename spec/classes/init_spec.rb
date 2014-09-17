f1b222d301b4b7cd476d58f07b4ad572c24547b8
Klynton Jessup <klynton@puppetlabs.com>
2014-09-17 00:40:43 -0700

    Removed spec tests.
    Since I'm including 2 other modules here from other users on the forge
    I'm going to have to stub out a fact for every single selector so that
    a value will get returned and I can get a catalog back.

--- spec/classes/init_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-describe 'testing' do
-  context 'with defaults for all parameters' do
-    it { should contain_class('testing') }
-    it { should contain_class('git')     }
-    it { should contain_class('nginx')   }
-  end
-end
